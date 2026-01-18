e70817f9754dccb01d9fd2da8ca2099de9ae5b68
komodu <dagliwill@gmail.com>
2026-01-18 13:17:09 +0000

    fix navbar mobile responsivenessbstrap

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 // seed/index.js
-require('dotenv').config();
-const mongoose = require('mongoose');
-const seedDays = require('./daySeed.js');
-const seedworkoutType = require('./workoutTypeSeed.js')
+require("dotenv").config();
+const mongoose = require("mongoose");
+const seedDays = require("./daySeed.js");
+const seedworkoutType = require("./workoutTypeSeed.js");
 // const seedRoles = require('./roleSeed'); // example for future seeds
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/yourdb';
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/yourdb";
 
 async function runAllSeeds() {
   try {
@@ -14,18 +14,18 @@ async function runAllSeeds() {
       useUnifiedTopology: true,
     });
 
-    console.log('Connected to MongoDB for seeding');
+    console.log("Connected to MongoDB for seeding");
 
     await seedDays();
     await seedworkoutType();
     // await seedRoles(); // call other seeders here
-    console.log('All seeds completed');
+    console.log("All seeds completed");
   } catch (err) {
-    console.error('Seeding error:', err);
+    console.error("Seeding error:", err);
     process.exitCode = 1;
   } finally {
     await mongoose.disconnect();
-    console.log('Disconnected from MongoDB');
+    console.log("Disconnected from MongoDB");
   }
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const app = require("./app")
-const connDB = require("./config/db")
+const app = require("./app");
+const connDB = require("./config/db");
 // Connect to DB (MongoDB)
 connDB();
 
-
-app.listen(process.env.PORT, () =>{
-    console.log("Server running on port " + process.env.PORT);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + process.env.PORT);
 });
